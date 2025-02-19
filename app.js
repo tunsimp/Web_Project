4520a7cfc09b91a08123bd340fe47fcaa257fab4
tunsimp <nguyenquoctuanwar@gmail.com>
2025-02-19 15:16:29 +0700

    login

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const app= express();
+const fs = require('fs');
+const _ = require('lodash');
+app.listen(3000);
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.sendFile('./views/index.html',{root:__dirname});
+});
+app.get('/about',(req,res)=>{
+    res.sendFile('./views/about.html',{root:__dirname});
+});
+app.get('/login',(req,res)=>{
+    res.sendFile('./views/login.html',{root:__dirname});
+});
+app.use((req,res)=>{
+    res.sendFile('./views/404.html',{root:__dirname});
+});
