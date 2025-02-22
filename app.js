b9a087516e553962a35af7b093ee0c0cfb735470
tunsimp <nguyenquoctuanwar@gmail.com>
2025-02-23 04:39:21 +0700

    view engine

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express= require('express');
-const app= express();
 const fs = require('fs');
 const _ = require('lodash');
+const app= express();
+
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(3000);
 app.use(express.static('public'));
+
+const stories= ['im 21 year old','i have an lovely family and girlfriend','i want to be an pentester'];
+
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+    res.render('index');
 });
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about',{stories:stories});
 });
 app.get('/login',(req,res)=>{
-    res.sendFile('./views/login.html',{root:__dirname});
+    res.render('login');
 });
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.status(404).render('404');  
 });
