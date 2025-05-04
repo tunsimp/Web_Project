ca08c77a5824c9e7a6f7898200465c4b384cee34
tunsimp <nguyenquoctuanwar@gmail.com>
2025-05-04 22:03:57 +0700

    Revert "small fix for route"
    This reverts commit c37b5c939cd0df53c3bd2e8950a8bea1f10fe2b1.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,6 @@ const checkAuth = require("../middleware/checkAuth");
 router.post("/login", authController.login); // Call the login controller
 router.post("/register", authController.register); // Call the register controller
 router.get("/check-auth", checkAuth, authController.getAuthStatus);
+
 router.post("/logout", authController.logout); // Call the logout controller
 module.exports = router;
