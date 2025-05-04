c37b5c939cd0df53c3bd2e8950a8bea1f10fe2b1
tunsimp <nguyenquoctuanwar@gmail.com>
2025-05-04 20:51:29 +0700

    small fix for route

--- backend/routes/lessonRoutes.js
+++ backend/routes/lessonRoutes.js
@@ -8,22 +8,18 @@ router.get("/content/:path(.+)", lessonController.getLessonPageContent); // Add 
 // Lesson routes
 router.get("/", lessonController.getAllLessons);
 router.get("/:id", lessonController.getLessonById);
-router.post("/", lessonController.createLesson);
+router.post("/create", lessonController.createLesson);
 router.put("/:id", lessonController.updateLesson);
 router.delete("/:id", lessonController.deleteLesson);
-
-// New route for getting lessons with user progress
-router.get("/user/progress", lessonController.getUserLessonsWithProgress);
-
 // Lesson page routes
 router.get("/:lessonId/pages/:pageNumber", lessonController.getLessonPage);
 router.get("/:lessonId/pages", lessonController.getAllLessonPages);
 router.post("/:lessonId/pages", lessonController.createLessonPage);
 router.put("/pages/:pageId", lessonController.updateLessonPage);
 router.delete("/pages/:pageId", lessonController.deleteLessonPage);
-router.post("/create", lessonController.createLesson);
 
 // // User progress routes
 router.post("/progress/:lessonId", lessonController.updateUserProgress);
+router.get("/user/progress", lessonController.getUserLessonsWithProgress);
 
 module.exports = router;
